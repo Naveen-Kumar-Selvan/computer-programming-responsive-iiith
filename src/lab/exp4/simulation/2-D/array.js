15b8c15de822cfbed0710110722725d2074c6a0f
Naveen-Kumar-Selvan <naveenkselvan@gmail.com>
2020-07-03 20:26:49 +0530

    demo

--- src/lab/exp4/simulation/2-D/array.js
+++ src/lab/exp4/simulation/2-D/array.js
@@ -119,9 +119,9 @@ window.view = {
 	},
 	generateFirstMatrixElements: function() {
 		var size = this.rowsA * this.colsA
-		for ( i = 1 ; i <= size ; i++)
+		for ( i = 0 ; i < 15 ; i++)
 		{
-			var random = i;
+			var random = math.Floor(math.Random * size);
 			this.matrixA.push(random)
 		}
 		this.resetRowsAndCols()
@@ -135,9 +135,9 @@ window.view = {
 	},
 	generateSecondMatrixElements: function() {
 		var size = this.rowsB * this.colsB
-		for ( i = 1 ; i <= size ; i++)
+		for ( i = 0 ; i < 15 ; i++)
 		{
-			var random = i;
+			var random = math.Floor(math.Random * size);
 			this.matrixB.push(random)
 		}
 		this.disableButton('generateB')
@@ -151,7 +151,7 @@ window.view = {
 		matA.className = 'table'
 		 // var caption = matA.createCaption();
 		 // caption.innerHTML = ""
-		for ( i = 1 ; i <= this.rowsA ; i++ )
+		for ( i = 0 ; i < this.rowsA ; i++ )
 		{	
 			var row = document.createElement('tr')
 			for ( j = 1 ; j <= this.colsA ; j++ )
@@ -174,10 +174,10 @@ window.view = {
 		matB.className = 'table'
 		// var caption = matB.createCaption();
 		// caption.innerHTML = "<b>Matrix B</b>"
-		for ( i = 1 ; i <= this.rowsB ; i++ )
+		for ( i = 0 ; i < this.rowsB ; i++ )
 		{	
 			var row = document.createElement('tr')
-			for ( j = 1 ; j <= this.colsB ; j++ )
+			for ( j = 0 ; j < this.colsB ; j++ )
 			{
 				var col = document.createElement('td')
 				col.className = 'matrixCell'
@@ -418,3 +418,16 @@ window.view = {
 	}
 }
 window.onload = function() { view.init() }
+
+$(document).ready(function(){
+		$("input[type='radio'][name=radio_group]").change(function(){
+		if($(this).val()=="enable")
+		{
+			$("#userInput").show();
+		}
+		else
+		{
+			$("#userInput").hide();
+		}
+		});
+	});
